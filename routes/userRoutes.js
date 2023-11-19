c126774d61ae7bfe18c3ced3ef5d3c7342353299
renanleitev <renanleitev@gmail.com>
2023-11-19 13:19:07 -0300

    Add: redefinePassword route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -110,6 +110,30 @@ router.patch('/:id', async (req, res) =>{
     }
 })
 
+//Redefinir senha
+router.patch('/forgetPassword', async (req, res) =>{
+
+    const { Email, Password } = req.body;
+
+    const user = await User.findOne({Email: Email});
+
+    try {
+
+        const updateUser = await User.updateOne({Password: Password}, user);
+
+        //verificar se o usuário existe
+        if(updateUser.matchedCount === 0){
+            res.status(422).json({ message: 'Usuário não foi encontrado!'});
+            return;
+        };
+
+        res.status(200).json(user);
+    } 
+    catch (error) {
+        res.status(500).json({error: error});        
+    }
+})
+
 router.delete('/:id', async (req, res) =>{
 
     const id = req.params.id;
