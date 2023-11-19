0fa03a1263ddda384a2a7446f40811208fa20672
renanleitev <renanleitev@gmail.com>
2023-11-19 11:08:00 -0300

    Fix: login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -170,7 +170,7 @@ router.get('/query/:param', async (req, res) => {
 router.post("/login", async function(req, res){ 
     try { 
         // check if the user exists 
-        const user = await User.findOne({ username: req.body.Username }); 
+        const user = await User.findOne({ Username: req.body.Username }); 
         if (user) { 
           //check if password matches 
           const result = req.body.Password === user.Password; 
