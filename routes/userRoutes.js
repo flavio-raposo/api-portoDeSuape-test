79f2c0e992847398a84402570f58138e0cab26d2
renanleitev <renanleitev@gmail.com>
2023-11-19 11:04:29 -0300

    Add: login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -165,5 +165,27 @@ router.get('/query/:param', async (req, res) => {
     }
 })
 
+// POST login
+//Handling user login 
+router.post("/login", async function(req, res){ 
+    try { 
+        // check if the user exists 
+        const user = await User.findOne({ username: req.body.Username }); 
+        if (user) { 
+          //check if password matches 
+          const result = req.body.Password === user.Password; 
+          if (result) { 
+            res.status(200).json(user);
+          } else { 
+            res.status(400).json({ error: "Senha incorreta" }); 
+          } 
+        } else { 
+          res.status(400).json({ error: "Usuário não existe" }); 
+        } 
+      } catch (error) { 
+        res.status(400).json({ error }); 
+      } 
+}); 
+
 //exportar para puxar no index
 module.exports = router;
